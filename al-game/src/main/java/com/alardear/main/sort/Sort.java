dcfc59afa0411ed9477bbe006b36a5c93a5d1510
zhengdi1992 <617094869@qq.com>
2017-04-05 20:30:02 +0800

    add base file

---
+++ al-game/src/main/java/com/alardear/main/sort/Sort.java
@@ -0,0 +1,6 @@
+package com.alardear.main.sort;
+
+public interface Sort {
+	void sort(long[] data);
+	void sort(long[] data, String filepath);
+}
---
+++ al-game/src/main/java/com/alardear/main/work/Worker.java
@@ -0,0 +1,13 @@
+package com.alardear.main.work;
+
+public class Worker implements Runnable{
+	
+	
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
